f4b1adcf6f1af604925d60042ec7c6cd04fa58c3
Harraki <abdallah.harraki@gmail.com>
2020-07-03 11:55:03 +0100

    storing the data in remote database 'mongodb atlas'

--- Bbc_crawler/project/pipelines.py
+++ Bbc_crawler/project/pipelines.py
@@ -5,9 +5,20 @@
 
 
 # useful for handling different item types with a single interface
+from pymongo import MongoClient
 from itemadapter import ItemAdapter
 
 
+
+
 class ProjectPipeline:
+
+    def __init__(self):
+        self.client = MongoClient("mongodb+srv://user1:kasdefghajklem@razinogriffith.r8vjd.mongodb.net/Articles?retryWrites=true&w=majority")
+        db=self.client['Articles']
+        self.collection=db["Articles_db"]
+        
+    
     def process_item(self, item, spider):
+        self.collection.insert(dict(item))
         return item
